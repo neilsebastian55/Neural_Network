ff4730abb520a0fae1d4ba370a5ddb249249b473
Neil Sebastian <neilsebastian@Neils-MacBook-Air.local>
2026-06-06 18:28:53 -0700

    initial commit - activation functions and network layer

---
+++ network_layer.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+class Layer:
+    def __init__(self, inputs, neurons, activation):
+        self.weights = np.random.randn(inputs, neurons) * 0.1
+        self.bias = np.zeros(1, neurons)
+        self.activation = activation
+    
+    def forward(self,x):
+        self.input = x
+        self.weighted_sum
